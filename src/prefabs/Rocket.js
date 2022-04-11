b36c17a5e7750da003bb1762c27608342d00848b
Andrew Wei <weiandrew2012@gmail.com>
2022-04-10 19:12:30 -0700

    commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
       scene.add.existing(this);
       this.isFiring = false;
       this.moveSpeed = 2;
+      this.scaleX = 2;
+      this.scaleY = 2;
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update() {
@@ -18,15 +21,20 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
 
-        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
         if (this.isFiring && this.y >= borderUISize*3+borderPadding) {
             this.y -= this.moveSpeed;
         }
         if (this.y <= borderUISize*3+borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -3,8 +3,32 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
+    preload() {
+        // load audio
+        this.load.audio('sfx_select', "./assets/Sounds/blip_select12.wav");
+        this.load.audio('sfx_explosion', './assets/Sounds/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/Sounds/rocket_shot.wav');
+    }
+
     create() {
         this.add.text(20, 20, "Rocket Patrol Menu");
+        let menuConfig = {
+            fontFamily: "Courier",
+            fontSize: "28px",
+            backgroundColor: "#F3B141",
+            color: "#843605",
+            align: "right",
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.add.text(game.config.width/2, game.config.height/2-borderUISize-borderPadding, "ROCKET PATROL", menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, "User arrows <--> to move & (F) to fire", menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = "#00FF00";
+        menuConfig.color = "#000";
+        this.add.text(game.config.width/2, game.config.height/2+borderUISize+borderPadding, "Press <- for Novice or -> for Expert", menuConfig).setOrigin(0.5);
         this.scene.start("playScene");
     }
 }
